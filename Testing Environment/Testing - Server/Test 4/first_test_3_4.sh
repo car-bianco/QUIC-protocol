044ca245421a123b243c706341c7e16aa749118d
car-bianco <carmine_bianco@hotmail.com>
2017-09-27 15:51:00 +0200

    Update first_test_3_4.sh

--- Testing Environment/Testing - Server/Test 4/first_test_3_4.sh
+++ Testing Environment/Testing - Server/Test 4/first_test_3_4.sh
@@ -18,3 +18,5 @@ sudo tc qdisc add dev ifb0 handle 1: root htb default 11
 sudo tc class add dev ifb0 parent 1: classid 1:1 htb rate 4Mbit
 sudo tc class add dev ifb0 parent 1:1 classid 1:11 htb rate 4Mbit
 sudo tc qdisc add dev ifb0 parent 1:11 handle 10: netem delay 10ms
+#create directory for test results
+mkdir ~/Documents/Testing/Test\ 1\ results/
